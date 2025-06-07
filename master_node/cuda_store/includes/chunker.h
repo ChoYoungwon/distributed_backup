e5625989e6883ad4379bdbdd69ab45ae08d2413e
Zel-da <anjongman1029@gmail.com>
2025-06-07 14:04:40 +0900

    ErrorFix

--- master_node/cuda_store/includes/chunker.h
+++ master_node/cuda_store/includes/chunker.h
@@ -1,8 +1,21 @@
 #ifndef CHUNKER_H
 #define CHUNKER_H
 
+#include <stdint.h>
+#include <stddef.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 void chunk_and_process_cuda(const char *filepath, const char *metadata_path);
+
 void process_chunk(uint8_t *chunk_buf, const uint8_t *data, size_t chunk_size, const char *metadata_path);
+
 void load_rabin_tables(uint64_t *table);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
